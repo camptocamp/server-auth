14fa091d0d6cedd593472a7f9ce157dcdb46065a
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-11-19 17:03:54 +0100

    Rename module wim_base to schweizmobil_base

---
+++ odoo/local-src/schweizmobil_base/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    'name': 'WIM Base',
+    'description': "Schweimobil PLUS Website - Base",
+    'version': '13.0.1.0.0',
+    'author': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'Others',
+    'depends': [
+        # odoo/src
+        "base",
+        "partner_firstname",
+    ],
+    'website': 'http://www.camptocamp.com',
+    'data': [
+        # views
+        "views/res_partner.xml",
+    ],
+    'installable': True,
+}
