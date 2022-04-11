b4663d3d4fde2925aa11fe0780718888a3bbd3a3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2022-04-11 13:40:57 +0200

    fixup! fixup! fixup! Refactoring of auth_saml module

--- auth_saml/tests/test_pysaml.py
+++ auth_saml/tests/test_pysaml.py
@@ -250,7 +250,7 @@ class TestPySaml(HttpCase):
         )
         # Assert that the user password can not be set
         with self.assertRaises(ValidationError):
-            user.password = "new password"
+            user.password = "New+Passw000rd"
 
     def test_disallow_user_password(self):
         """Test that existing user password is deleted when adding an SAML provider when
