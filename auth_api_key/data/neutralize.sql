3d5193f8b571951e9961c1fde384f883803634d2
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2026-06-12 09:31:51 +0200

    Disable auth_api_key neutralization
    We need the api keys to tests the system.
    And there is no easy way to disable it by module, that I know of.

--- auth_api_key/data/neutralize.sql
+++ auth_api_key/data/neutralize.sql
@@ -1,4 +1,7 @@
 -- deactivate all api keys and clear the keys
-UPDATE auth_api_key
-   SET active = false,
-       key = 'neutralized';
+
+-- We need the key for testing - deactivating the neutralization
+
+-- UPDATE auth_api_key
+--    SET active = false,
+--        key = 'neutralized';
